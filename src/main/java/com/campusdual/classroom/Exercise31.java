09918b3378846aa692acb5d90a34b38a0dfe50bb
Lucía Parrilla del Río <luciaparrilla9@hotmail.com>
2024-10-31 17:27:15 +0100

    Terminado 31

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,23 @@
 package com.campusdual.classroom;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
-
+        Path path = Paths.get("src/main/resources/lorem.txt");
+        try {
+            List<String> lines = Files.readAllLines(path);
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("We couldn't read Lorem ipsum." );
+        }
     }
 }
+
